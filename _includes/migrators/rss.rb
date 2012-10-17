c8b75e57d52ee022e82b041bc851849ca01d6038
Hannes Lilljequist <hannes@sthlmconnection.se>
2012-10-17 08:10:25 +0200

    Fixed encoding problems in file names and titles.

--- _includes/migrators/rss.rb
+++ _includes/migrators/rss.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 # Custom version of Jekyll's RSS migrator.
 #
 # Usage:
@@ -18,6 +20,39 @@ require 'yaml'
 module Jekyll
   module MigrateRSS
 
+    # Make a suitable file name from a title by removing whitespace and special characters.
+    def self.filename(title)
+      title.split(%r{ |!|/|:|&|-|$|,|\?|%|\(|\)}).map { |i| i.downcase if i != '' }.compact.join('-')
+    end
+
+    # Convert to ascii and replace non-ascii letters.
+    def self.transliterate(input)
+      replacements = {
+        'å' => 'a',
+        'ä' => 'a',
+        'â' => 'a',
+        'á' => 'a',
+        'à' => 'a',
+        'ç' => 'c',
+        'é' => 'e',
+        'è' => 'e',
+        'ê' => 'e',
+        'ë' => 'e',
+        'ï' => 'i',
+        'í' => 'i',
+        'ì' => 'i',
+        'ö' => 'o',
+        'ó' => 'o',
+        'ò' => 'o',
+        'ô' => 'o',
+        'ü' => 'u',
+        'æ' => 'ae',
+        'œ' => 'oe',
+        'ß' => 'ss',
+      }
+      input.encode! Encoding::ASCII, :fallback => replacements
+    end
+
     # The `source` argument may be a URL or a local file.
     def self.process(source)
       content = ""
@@ -27,17 +62,22 @@ module Jekyll
       raise "There doesn't appear to be any RSS items at the source (#{source}) provided." unless rss
 
       rss.items.each do |item|
+
+        post_name = self.filename(item.title)
         formatted_date = item.date.strftime('%Y-%m-%d')
-        post_name = item.title.split(%r{ |!|/|:|&|-|$|,}).map { |i| i.downcase if i != '' }.compact.join('-')
-        name = "#{formatted_date}-#{post_name}"
+        file_name = "#{formatted_date}-#{post_name}"
+
+        permalink = self.transliterate(post_name).sub('?', '') # Remove remaining question marks.
+
         content = HTMLPage.new :contents => item.description
 
         header = {
           'layout' => 'post',
-          'title' => item.title
+          'title' => item.title.gsub(/&(?!amp;)/i, '&amp;'),
+          'permalink' => permalink
         }
 
-        File.open("_posts/#{name}.md", "w") do |f|
+        File.open("_posts/#{file_name}.md", "w") do |f|
           f.puts header.to_yaml
           f.puts "---\n"
           f.puts content.markdown
